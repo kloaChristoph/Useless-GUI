f38a72ef3181d57e58992fa63cb2af68df89af90
kloaChristoph <christoph.alexander.holas@gmail.com>
2023-03-11 00:25:04 +0100

    commenting App class

--- client/game.py
+++ client/game.py
@@ -6,11 +6,113 @@ import time
 from client_network import NetworkClient
 
 class App:
+    """
+    A class to handle the window ant the game
+
+    ...
+
+    Attributes
+    ----------
+    client : NetworkClient
+        The network part of the client
+    window : tkinter.TK
+        The window which is shown to the user
+    username : str
+        The name of the user
+
+    login_status : bool
+        A variable to check the if user is loged in 
+    game_running : bool
+        A variable to check if there is a game running
+
+    score: int
+        The score of the last game
+    missed_cliscks: int
+        The wrong presses of the last game
+    accuracy : float
+        The calculated accuracy the user had in the last game [%]
+    highscore : int 
+        The all time highscore of the user
+    highscore_accuracy : float
+        The accuracy the user had when reaching the highscore
+
+    exit_button : tkinter.Button
+        Sends the server a command to close the connection if loged in and closes the window (calls exit_app)
+    start_button : tkinter.Button
+        Starts a new game if there is no game running (calls start_game)
+    time_label : tkinter.Label
+        Show the time which is left in the game
+    login_button : tkinter.Button
+        Tries to log in (calls login)
+        Shows login screen if user is in register screen (calls login_window)
+    register_button : tkinter.Button
+        Tries to register and log in (calls register)
+        Shows register screen if user is in login screen (calls register_window)
+    username_label : tkinter.Label
+        Shows where to type the username in the login/register window
+    username_entry : tkinter.Entry
+        Allows the user to type in the username in the login/register window
+    password_label : tkinter.Label
+        Shows where to type the password in the login/register window
+    password_entry : tkinter.Entry
+        Allows the user to type in the username in the login/register window
+    password_confirm_label : tkinter.Label
+        Shows where to confirm the typed password in the register window
+    password_confirm_entry : tkinter.Entry
+        Allows the user to conf
+    highscore_talbe : ttk.Treeview
+        The table to show the highscores of the top 10 users
+    buttons : list[GameButton]
+        A list of the game buttons (each calls GameButton.check_status)
+    
+    Methods
+    -------
+    start_loop() -> None
+        Starts the mainloop to update the window
+    handle_server_commands() -> None
+        Handle the commands received from the server
+    login_window() -> None
+        Changes the Window to the login screen
+    login() -> None
+        Method triggerd by the login button when user is in the login screen
+        Calls the setup_connection method and destroys the widgets that are no longer needed
+    register_window() -> None
+        Changes the Window to the register screen
+    register() -> None
+        Method triggerd by the register button when user is in the register screen
+        Calls the setup_connection method and destroys the widgets that are no longer needed
+        The user is loged in automatically
+    setup_connection(register: bool) -> None
+        Tries to connect to server and gives pop up if connection to server failed or the login wasn't correct
+    start_game() -> None
+        Starts the game if there is no game running
+        Is triggert by the start button in the main window
+    show_end_msg() -> None
+        Shows a pop up at the end of the game
+        Shows the reached score and if it is a new highscore
+    create_table() -> None
+        Creats the highscore table
+    update_highscore_table(data: dict) -> None
+        Updates the highscore table
+    show_buttons() -> None
+        Creating and showing the buttons needed for the game
+    calculate_score() -> None
+        Iterates through all game buttons and gets the number of the correct and false clicks the user made
+    calculate_accuracy() -> None
+        Calculates the accuracy of the last game
+    reset_stats() -> None
+        Resets the current game stats
+    exit_app() -> None
+        Show pop up if user really want's to exit the app
+        Sends the server a command to close the connection
+        Closes the window
+    """
+
     def __init__(self, window: tkinter.Tk, window_title: str) -> None:
         self.client = NetworkClient()
 
-        self.window = window
-        self.login_status = False
+        self.window: tkinter.Tk = window
+        self.login_status: bool = False
         self.username: str = ""
 
         self.exit_button = tkinter.Button(self.window, name="exit_button", text="Exit", command=self.exit_app, height=2, width=10)
@@ -20,12 +122,12 @@ class App:
 
         self.window.title(window_title)
         self.window.geometry("700x500")
-        self.game_running = False
-        self.score = 0
-        self.missed_clicks = 0
-        self.highscore = 0
-        self.accuracy = 100
-        self.highscore_accuracy = 0
+        self.game_running: bool = False
+        self.score: int = 0
+        self.missed_clicks: int = 0
+        self.accuracy: float = 100
+        self.highscore: int = 0
+        self.highscore_accuracy: float = 0
 
         self.create_table()
         self.client.send_to_server("REQUEST_HIGHSCORE_TABLE",self.username)
@@ -39,7 +141,7 @@ class App:
         self.start_loop()
 
 
-
+#------------------------MAINLOOP-----------------------#
     def start_loop(self) -> None:
         """
         Starts the loop that updates the Window
@@ -76,8 +178,10 @@ class App:
             match recv.get("command"):
                 case "UPDATE_HIGHSCORE_TABLE":
                     self.update_highscore_table(recv)
+#------------------------MAINLOOP-----------------------#
 
 
+#-------------------------LOGIN-------------------------#
     def login_window(self) -> None:
         """
         Changes the Window to the login screen
@@ -123,29 +227,6 @@ class App:
             self.window.update()
 
 
-    def register_window(self) -> None:
-        """
-        Changes the Window to the register screen
-        Is triggert if client presses on register button in login screen
-
-        Parameters
-        ----------
-        None
-
-        Returns
-        -------
-        None
-        """
-        self.login_button.config(command=self.login_window)
-        self.register_button.config(command=self.register)
-
-        self.password_confirm_label = tkinter.Label(self.window, name="confirm_label", text="Confirm:", height=3, width=10, font=('Arial 15'))
-        self.password_confirm_label.place(x=10, y=230)
-
-        self.password_confirm_entry = tkinter.Entry(self.window, show="*", name="confirm_entry", font=('Arial 15'))
-        self.password_confirm_entry.place(x=120, y=255)
-
-
     def login(self) -> None:
         """
         Method triggerd by the login button when user is in the login screen
@@ -170,7 +251,30 @@ class App:
             self.password_label.destroy()
 
 
-    def register(self):
+    def register_window(self) -> None:
+        """
+        Changes the Window to the register screen
+        Is triggert if client presses on register button in login screen
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+        self.login_button.config(command=self.login_window)
+        self.register_button.config(command=self.register)
+
+        self.password_confirm_label = tkinter.Label(self.window, name="confirm_label", text="Confirm:", height=3, width=10, font=('Arial 15'))
+        self.password_confirm_label.place(x=10, y=230)
+
+        self.password_confirm_entry = tkinter.Entry(self.window, show="*", name="confirm_entry", font=('Arial 15'))
+        self.password_confirm_entry.place(x=120, y=255)
+
+
+    def register(self) -> None:
         """
         Method triggerd by the register button when user is in the register screen
         Calls the setup_connection method and destroys the widgets that are no longer needed
@@ -228,9 +332,74 @@ class App:
         
             elif status == True:
                 self.login_status = True
+#-------------------------LOGIN-------------------------#
 
 
-    def create_table(self)-> None:
+#-------------------------GAME--------------------------#
+    def start_game(self) -> None:
+        """
+        Starts the game
+        Is triggert by the start button in the main window
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+        duration = 5
+        #Checking if there is already a game and starts one if there is not
+        if not self.game_running:
+            self.reset_stats()
+            start_time = time.time()
+            self.game_running = True
+
+            #Runs the game as long as the timer is runnning
+            while start_time + duration >= time.time():
+                random_button: GameButton = random.choice(self.buttons)
+                random_button.change_color()
+
+                #Updates the screen and waits until the right button is pressed
+                while random_button.highlighted == True and start_time + duration >= time.time():
+                    time_left = str(int(duration-(time.time()-start_time)))
+                    self.time_label.config(text=f"Time: {time_left}")
+                    self.window.update()
+                
+            self.calculate_score()
+            self.game_running = False
+            random_button.reset_button()
+            self.show_end_msg()
+
+    def show_end_msg(self) -> None:
+        """
+        Shows a pop up at the end of the game
+        Shows the reached score and if it is a new highscore
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+        self.calculate_accuracy()
+        if self.score > self.highscore:
+            self.highscore = self.score
+            self.highscore_accuracy = self.accuracy
+            tkinter.messagebox.showinfo(title="NEW HIGHSCORE", \
+                                                        message=f"Congratulation, you reached a new highscore!\
+                                                        \nYour highscore: {self.highscore} with an accuracy of {self.highscore_accuracy}%")
+        else:
+            tkinter.messagebox.showinfo(title="Game stats", message=f"Your score: {self.score} with an accuracy of {self.accuracy}%\
+                                         \nYour highscore: {self.highscore} with an accuracy of {self.highscore_accuracy}%")
+#-------------------------GAME--------------------------#
+
+
+#------------------------OTHER--------------------------#
+    def create_table(self) -> None:
         """
         Creats the highscore table
 
@@ -278,27 +447,6 @@ class App:
             self.highscore_talbe.insert("", tkinter.END, values=row)
         
 
-
-    def exit_app(self) -> None:
-        """
-        Show pop up if user really want's to exit the app
-        Sends the server a command to close the connection
-        Closes the window
-
-        Parameters
-        ----------
-        None
-
-        Returns
-        -------
-        None
-        """
-        if tkinter.messagebox.askyesno(title="EXIT", message="Do you really want to exit the app?"):
-            self.client.send_to_server("CLOSE_CONNECTION", self.username)
-            self.window.destroy()
-            self.client.server.close()
-
-
     def show_buttons(self) -> None:
         """
         Creating the buttons needed for the game
@@ -317,10 +465,10 @@ class App:
         y_pos = [50, 136, 222]
         for y in y_pos:
             for x in x_pos:
-                self.buttons.append(GameButton(self.window, x, y, str(len(self.buttons)+1)))
+                self.buttons.append(GameButton(self.window, x = x, y = y, text = str(len(self.buttons)+1)))
         
 
-    def get_score(self) -> None:
+    def calculate_score(self) -> None:
         """
         Iterates through all game buttons and gets the number of the correct and false clicks the user made
 
@@ -336,62 +484,6 @@ class App:
             self.score += button.right_pressed
             self.missed_clicks += button.false_pressed
     
-
-    def reset_stats(self) -> None:
-        """
-        Resets the current game stats
-
-        Parameters
-        ----------
-        None
-
-        Returns
-        -------
-        None
-        """
-        self.score = 0
-        self.missed_clicks = 0
-        for button in self.buttons:
-            button.right_pressed = 0
-            button.false_pressed = 0
-
-
-    def start_game(self) -> None:
-        """
-        Starts the game
-        Is triggert by the start button in the main window
-
-        Parameters
-        ----------
-        None
-
-        Returns
-        -------
-        None
-        """
-        duration = 5
-        #Checking if there is already a game and starts one if there is not
-        if not self.game_running:
-            self.reset_stats()
-            start_time = time.time()
-            self.game_running = True
-
-            #Runs the game as long as the timer is runnning
-            while start_time + duration >= time.time():
-                random_button: GameButton = random.choice(self.buttons)
-                random_button.change_color()
-
-                #Updates the screen and waits until the right button is pressed
-                while random_button.highlighted == True and start_time + duration >= time.time():
-                    time_left = str(int(duration-(time.time()-start_time)))
-                    self.time_label.config(text=f"Time: {time_left}")
-                    self.window.update()
-                
-            self.get_score()
-            self.game_running = False
-            random_button.reset_button()
-            self.show_end_msg()
-
 
     def calculate_accuracy(self) -> None:
         """
@@ -411,12 +503,11 @@ class App:
 
         except ZeroDivisionError:
             self.accuracy = 0
-            
+ 
 
-    def show_end_msg(self) -> None:
+    def reset_stats(self) -> None:
         """
-        Shows a pop up at the end of the game
-        Shows the reached score and if it is a new highscore
+        Resets the current game stats
 
         Parameters
         ----------
@@ -426,18 +517,32 @@ class App:
         -------
         None
         """
-        self.calculate_accuracy()
-        if self.score > self.highscore:
-            self.highscore = self.score
-            self.highscore_accuracy = self.accuracy
-            tkinter.messagebox.showinfo(title="NEW HIGHSCORE", \
-                                                        message=f"Congratulation, you reached a new highscore!\
-                                                        \nYour highscore: {self.highscore} with an accuracy of {self.highscore_accuracy}%")
-        else:
-            tkinter.messagebox.showinfo(title="Game stats", message=f"Your score: {self.score} with an accuracy of {self.accuracy}%\
-                                         \nYour highscore: {self.highscore} with an accuracy of {self.highscore_accuracy}%")
+        self.score = 0
+        self.missed_clicks = 0
+        for button in self.buttons:
+            button.right_pressed = 0
+            button.false_pressed = 0
+       
 
+    def exit_app(self) -> None:
+        """
+        Show pop up if user really want's to exit the app
+        Sends the server a command to close the connection
+        Closes the window
 
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+        if tkinter.messagebox.askyesno(title="EXIT", message="Do you really want to exit the app?"):
+            self.client.send_to_server("CLOSE_CONNECTION", self.username)
+            self.window.destroy()
+            self.client.server.close()
+#------------------------OTHER--------------------------#
 
 class GameButton:
     """
